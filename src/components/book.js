a5d39dfa9ac8731879a35cb0b4e8e43121ed287e
Eyüp Sercan UYGUR <sercanuygur@gmail.com>
2020-12-28 16:32:54 +0300

    Style BookList

--- src/components/book.js
+++ src/components/book.js
@@ -1,47 +1,52 @@
 import React from 'react';
 import '../App.css';
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 
 const book = props => {
   const bookObjForBook = props.bookObj.bookObj;
   let i = 0;
 
   return (
-    <>
+    <div className="book-container-container">
       <h2 id="book-list-heading">The book List</h2>
 
       {bookObjForBook.filter(element => (!props.selectedFilter || props.selectedFilter === 'All' ? element : element.category === props.selectedFilter)).map(book => {
         i += 1;
         return (
-          <div key={i} className="flex">
-            <div>
-              <div>{book.category}</div>
-              <div>{book.title}</div>
-              <div>{book.author}</div>
+          <div key={i}>
+            <div className="flex book-container">
               <div>
-                <button type="button">Comment</button>
-                <button onClick={() => props.handleRemove(book)} type="button">Remove</button>
-                <button type="button">Edit</button>
+                <div id="book-category">{book.category}</div>
+                <div id="book-title">{book.title}</div>
+                <div className="blue">{book.author}</div>
+                <div id="button-holder">
+                  <button className="blue no-border" type="button">Comment</button>
+                  <button onClick={() => props.handleRemove(book)} className="blue no-border" type="button">Remove</button>
+                  <button className="blue no-border" type="button">Edit</button>
+                </div>
               </div>
-            </div>
-            <div>
-              Placeholder for status bar
-            </div>
-            <div>
-              {book.read}
+              <div className="Oval-2">
+                <CircularProgressbar value={book.read} className="circular" />
+              </div>
               <div>
-                Completed
+                {`${book.read} %`}
+                <div>
+                  Completed
+                </div>
               </div>
-            </div>
-            <div>
-              <h4>Current Chapter</h4>
-              <p>Chapter XXX</p>
-              <button type="button">UPDATE PROGRESS</button>
+              <div className="chapter">
+                <h4>Current Chapter</h4>
+                <p>Chapter XXX</p>
+                <button type="button" id="update-button">UPDATE PROGRESS</button>
+              </div>
             </div>
           </div>
+
         );
       })}
 
-    </>
+    </div>
   );
 };
 export default book;
